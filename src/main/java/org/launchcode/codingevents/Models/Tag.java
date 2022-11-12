0e1c1e4046b89dda84ebab82a295427b1735d5a6
Ashley <aforrest56@gmail.com>
2022-11-11 21:21:39 -0600

    add-tags set up

---
+++ src/main/java/org/launchcode/codingevents/Models/Tag.java
@@ -0,0 +1,30 @@
+package org.launchcode.codingevents.Models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Tag extends AbstractEntity{
+
+    @Size(min = 1, max = 25)
+    @NotBlank
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName(){
+        return "#" + name + " ";
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -79,6 +79,21 @@ public class EventController {
         return "redirect:";  //redirects to index
     }
 
+    @GetMapping("detail")
+    public String displayEventDetails(@RequestParam Integer eventId, Model model){
+        Optional<Event> result = eventRepository.findById(eventId);
+
+        if(result.isEmpty()){
+            model.addAttribute("title", "Invalid Event Id: " + eventId);
+        } else {
+            Event event = result.get();
+            model.addAttribute("title", event.getName() + "Details");
+            model.addAttribute("event", event);
+        }
+        return "events/detail";
+    }
+
+
   /*  @GetMapping("edit/{eventId}")
     public String displayEditForm(Model model, @PathVariable int eventId){
         Event eventToEdit = EventData.getById(eventId);
